b60253fe145876ac5c998f5bd70724e5757cafa3
Vishu1910 <63246619+Vishu1910@users.noreply.github.com>
2020-04-06 22:32:26 +0530

    Assigning elements in a list

---
+++ list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+list=[1,2,3,4]
+print(list)
+list.append(5)
+print(list)
+list2=[6,7,8,9]
+print(list+list2)
+print(list*2)
+
+
+# In[ ]:
+
+
+
+
