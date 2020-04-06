75a7cf7d526d8f52688c20cfb15e4f769a68fb71
Vishu1910 <63246619+Vishu1910@users.noreply.github.com>
2020-04-06 22:28:36 +0530

    Deleting elements in dict

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+dict={"A":1,"B":2,"C":3,"D":4}
+print("Dictionary without deleting",dict)
+del dict["A"]
+print("Dictionary after deleting",dict)
+
+
+# In[ ]:
+
+
+
+
