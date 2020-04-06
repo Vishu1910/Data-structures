1ad444b4a94e42e95c53647b8cb81459433ad9a2
Vishu1910 <63246619+Vishu1910@users.noreply.github.com>
2020-04-06 22:23:29 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+tup1=('c','c++','java','python')
+tup2=('1','2','3','4','5')
+print("Tuple1 first element",tup1[0])
+print("Tuple1 second element",tup1[1])
+print("Tuple1 third element",tup1[2])
+print("Tuple2 elements 2-5",tup2[1:])
+
+
+# In[ ]:
+
+
+
+
